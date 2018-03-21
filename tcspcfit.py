bdafc4ff9a351da42595d29c92aa11cbeadb3ab6
bertus <bertus.mooikloof@gmail.com>
2018-03-21 22:20:23 +0200

    convol now works for multidimensional x - was only tested for single dimension before previous commit.

--- tcspcfit.py
+++ tcspcfit.py
@@ -20,24 +20,33 @@ def convol(irf, x):
     if x.ndim == 1:
         irf = irf.flatten()
         x = x.flatten()
-
-    p = np.size(x, 0)
+        p = np.size(x,0)
+    else:
+        p = np.size(x, 1)
+    print(p)
     n = np.size(irf, 0)
     if p > n:
         irf = np.append(irf, mm * np.ones(p - n))
     else:
         irf = irf[0:p]
-
+    print(np.size(irf), np.size(x))
     print(np.ones(p), fft(irf))
-    y = np.float_(ifft((fft(irf) * np.ones(p)) * fft(x)))
+    if x.ndim == 1:
+        y = np.float_(ifft((fft(irf) * np.ones(p)) * fft(x)))
+    else:
+        print(irf)
+        y = np.float_(ifft(np.outer(fft(irf), np.ones(np.size(x, 0))) * np.transpose(fft(x))))
 
     # t has the same length as irf:
     if n <= p:
         t = np.arange(0, n - 1)
     else:
         # print(np.arange(0, p-1), np.arange(0, n-p))
-        t = np.concatenate((np.arange(0, p - 1), np.arange(0, n - p)))
+        print(n)
+        t = np.concatenate((np.arange(0, p - 2), np.arange(0, n - p - 1)))
+        print(t)
 
     y = y.take(t, 0)
+    y = np.transpose(y)
     return y
 
--- testconvol.py
+++ testconvol.py
@@ -2,11 +2,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 from tcspcfit import convol
 
-x = np.array([0, 0, 0, 0, 0, 1, 2, 3, 3, 3, 0, 0, 0, 0, 0, 0])
-irf = np.array([1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1])
+x = np.array([[0, 0, 0, 0, 0, 1, 2, 3, 3, 3, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 2, 3, 3, 3, 3, 3, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 2, 3, 3, 3, 3, 3, 0, 0, 0, 0]])
+#x = np.array([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ,0])
+irf = np.array([1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0])
 
 y = convol(irf, x)
-
+print(y)
 plt.plot(irf)
 plt.plot(x)
 plt.plot(y)
