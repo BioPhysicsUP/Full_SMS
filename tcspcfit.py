299750525fe1a9b779eda5c21d22e5024af11fe9
bertus <bertus.mooikloof@gmail.com>
2018-03-23 10:22:44 +0200

    Properly tested convol, now really works 100%. Added comments, made test code clear.

--- tcspcfit.py
+++ tcspcfit.py
@@ -9,6 +9,7 @@ from statistics import *
 import numpy as np
 from scipy.fftpack import fft, ifft
 
+bug = 'bug'
 
 def convol(irf, x):
     """Performs a convolution of irf with x.
@@ -18,35 +19,33 @@ def convol(irf, x):
 
     mm = mean(irf[-11:-1])
     if x.ndim == 1:
-        irf = irf.flatten()
-        x = x.flatten()
+        # irf = irf.flatten()  # Needed in matlab, apparently not with numpy
+        # x = x.flatten()
         p = np.size(x,0)
     else:
-        p = np.size(x, 1)
-    print(p)
+        p = np.size(x, 1)  # There should be a way to remove this if..else , maybe transpose the inputs
     n = np.size(irf, 0)
     if p > n:
         irf = np.append(irf, mm * np.ones(p - n))
     else:
         irf = irf[0:p]
-    print(np.size(irf), np.size(x))
-    print(np.ones(p), fft(irf))
-    if x.ndim == 1:
+    if x.ndim == 1:  # Again this if..else shouldn't be necessary
         y = np.float_(ifft((fft(irf) * np.ones(p)) * fft(x)))
     else:
-        print(irf)
-        y = np.float_(ifft(np.outer(fft(irf), np.ones(np.size(x, 0))) * np.transpose(fft(x))))
+        print(fft(irf))
+        print(np.outer(fft(irf), np.ones(np.size(x, 0))))
+        y = np.float_(ifft(np.outer(np.ones(np.size(x, 0)), fft(irf)) * fft(x)))
 
-    # t has the same length as irf:
+    # t needs to have the same length as irf (n) in each case:
     if n <= p:
-        t = np.arange(0, n - 1)
+        t = np.arange(0, n - 2)
     else:
-        # print(np.arange(0, p-1), np.arange(0, n-p))
-        print(n)
         t = np.concatenate((np.arange(0, p - 2), np.arange(0, n - p - 1)))
-        print(t)
 
-    y = y.take(t, 0)
-    y = np.transpose(y)
+    # Either remove from y or add from start to end so that y has same length as irf.
+    if x.ndim == 1:  # This one should also go away.
+        y = y.take(t, 0)
+    else:
+        y = y.take(t, 1)
     return y
 
--- testconvol.py
+++ testconvol.py
@@ -1,14 +1,22 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from tcspcfit import convol
+from scipy.fftpack import fft, ifft
 
-x = np.array([[0, 0, 0, 0, 0, 1, 2, 3, 3, 3, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 2, 3, 3, 3, 3, 3, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 2, 3, 3, 3, 3, 3, 0, 0, 0, 0]])
-#x = np.array([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ,0])
-irf = np.array([1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0])
+x1 = np.array([[0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0]])
+x2 = np.array([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0])
+irf = np.array([0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0])
 
-y = convol(irf, x)
-print(y)
+y1 = convol(irf, x1)
+y2 = convol(irf, x2)
 plt.plot(irf)
-plt.plot(x)
-plt.plot(y)
+plt.plot(x2)
+plt.plot(y2)
+plt.plot(ifft(fft(irf)*fft(x2)))  # Testing to see whether the plots overlap
+plt.plot(x1[1])
+plt.plot(y1[1])
+plt.plot(ifft(fft(irf)*fft(x1[1])))  # Works for multi-dimensional x
 plt.show()
+
+
