3948dc1e8298555bcebca5635a8dee2237317c9f
bertus <bertus.mooikloof@gmail.com>
2019-02-28 14:48:01 +0200

    Changed documentation theme

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2019, Bertus van Heerden, Joshua Botha'
 author = 'Bertus van Heerden, Joshua Botha'
 
 # The short X.Y version
-version = ''
+version = '0.l'
 # The full version, including alpha/beta/rc tags
 release = '0.1'
 
@@ -80,7 +80,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
