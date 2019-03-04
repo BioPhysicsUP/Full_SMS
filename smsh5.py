e01ff228832772fbdecbc8e1b40cdea82c1aadfa
bertus <bertus.mooikloof@gmail.com>
2019-03-04 15:12:23 +0200

    Started working on hdf5 backend.

--- smsh5.py
+++ smsh5.py
@@ -9,16 +9,58 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+class H5dataset:
+
+    def __init__(self, filename):
+
+        self.file = h5py.File(filename, 'r')
+        self.particles = []
+        for particlename in self.file.keys():
+            self.particles.append(Particle(particlename, self))
+        self.numpart = len(self.particles)
+        assert self.numpart == self.file.attrs['# Particles']
+        self.channelwidth = None
+
+    def makehistograms(self):
+        """Put the arrival times into a histogram"""
+
+        for particle in self.particles:
+            tmin = min(self.tmin, particle.microtimes[:].min())
+            tmax = max(self.tmax, particle.microtimes[:].max())
+            window = tmax - tmin
+            numpoints = int(window // self.channelwidth)
+
+            t = np.linspace(0, window, numpoints)
+            # particle.microtimes -= particle.microtimes.min()
+
+            particle.measured, particle.t = np.histogram(particle.microtimes[:], bins=t)
+
+    def binints(self, binsize):
+
+        for particle in self.particles:
+            data = particle.abstimes[:]
+
+            binsize = binsize * 1000000
+            endbin = np.int(np.max(data) / binsize)
+
+            binned = np.zeros(endbin)
+            for step in range(endbin):
+                binned[step] = np.size(data[((step + 1) * binsize > data) * (data > step * binsize)])
+
+            binned *= (1000 / 100)
+            particle.binned = binned
+
+
 class Particle:
 
-    def __init__(self, name, file, number, irf, tmin, tmax, channelwidth=None):
+    def __init__(self, name, dataset):#, number, irf, tmin, tmax, channelwidth=None):
 
         self.name = name
-        self.file = file
-        self.datadict = file[self.name]
-        self.microtimes = self.datadict['Micro Times (s)'][:]
-        self.abstimes = self.datadict['Absolute Times (ns)'][:]
-        self.spectra = self.datadict['Spectra (counts\s)'][:, :]
+        self.dataset = dataset
+        self.datadict = self.dataset.file[self.name]
+        self.microtimes = self.datadict['Micro Times (s)']
+        self.abstimes = self.datadict['Absolute Times (ns)']
+        self.spectra = self.datadict['Spectra (counts\s)']
         self.wavelengths = self.datadict['Spectra (counts\s)'].attrs['Wavelengths']
         self.spectratimes = self.datadict['Spectra (counts\s)'].attrs['Spectra Abs. Times (s)']
         # fig, ax = plt.subplots()
@@ -31,27 +73,34 @@ class Particle:
         except KeyError:
             print("Problem loading raster scan for " + self.name)
         self.description = self.datadict.attrs['Discription']
-        self.irf = irf
-        if channelwidth is None:
-            differences = np.diff(np.sort(self.microtimes))
-            channelwidth = np.unique(differences)[1]
-        self.channelwidth = channelwidth
-        self.tmin = tmin
-        self.tmax = tmax
+        # self.irf = irf
+        # if channelwidth is None:
+        #     differences = np.diff(np.sort(self.microtimes[:]))
+        #     channelwidth = np.unique(differences)[1]
+        # self.channelwidth = channelwidth
+        # self.tmin = tmin
+        # self.tmax = tmax
         self.measured = None
         self.t = None
         self.ignore = False
         self.bg = False
 
-    def makehistogram(self):
-        """Put the arrival times into a histogram"""
 
-        self.tmin = min(self.tmin, self.microtimes.min())
-        self.tmax = max(self.tmax, self.microtimes.max())
-        window = self.tmax - self.tmin
-        numpoints = int(window // self.channelwidth)
+class Trace:
+    pass
 
-        t = np.linspace(0, window, numpoints)
-        self.microtimes -= self.microtimes.min()
 
-        self.measured, self.t = np.histogram(self.microtimes, bins=t)
+class RasterScan:
+    pass
+
+
+class Spectra:
+    pass
+
+
+class Histogram:
+    pass
+
+
+class Levels:
+    pass
