1dc500907af0abf2b48aa73a4b12928edf404531
bertus <bertus.mooikloof@gmail.com>
2019-03-05 20:01:26 +0200

    Added functionality to bin intensity traces and lifetime histograms. Ensured that raw data is not stored in memory.

--- smsh5.py
+++ smsh5.py
@@ -22,38 +22,21 @@ class H5dataset:
         self.channelwidth = None
 
     def makehistograms(self):
-        """Put the arrival times into a histogram"""
+        """Put the arrival times into histograms"""
 
         for particle in self.particles:
-            tmin = min(self.tmin, particle.microtimes[:].min())
-            tmax = max(self.tmax, particle.microtimes[:].max())
-            window = tmax - tmin
-            numpoints = int(window // self.channelwidth)
-
-            t = np.linspace(0, window, numpoints)
-            # particle.microtimes -= particle.microtimes.min()
-
-            particle.measured, particle.t = np.histogram(particle.microtimes[:], bins=t)
+            particle.makehistogram()
 
     def binints(self, binsize):
+        """Bin the absolute times into traces using binsize"""
 
         for particle in self.particles:
-            data = particle.abstimes[:]
-
-            binsize = binsize * 1000000
-            endbin = np.int(np.max(data) / binsize)
-
-            binned = np.zeros(endbin)
-            for step in range(endbin):
-                binned[step] = np.size(data[((step + 1) * binsize > data) * (data > step * binsize)])
-
-            binned *= (1000 / 100)
-            particle.binned = binned
+            particle.binints(binsize)
 
 
 class Particle:
 
-    def __init__(self, name, dataset):#, number, irf, tmin, tmax, channelwidth=None):
+    def __init__(self, name, dataset, tmin, tmax, channelwidth):#, number, irf, tmin, tmax, channelwidth=None):
 
         self.name = name
         self.dataset = dataset
@@ -74,20 +57,73 @@ class Particle:
             print("Problem loading raster scan for " + self.name)
         self.description = self.datadict.attrs['Discription']
         # self.irf = irf
-        # if channelwidth is None:
-        #     differences = np.diff(np.sort(self.microtimes[:]))
-        #     channelwidth = np.unique(differences)[1]
-        # self.channelwidth = channelwidth
-        # self.tmin = tmin
-        # self.tmax = tmax
+        if channelwidth is None:
+            differences = np.diff(np.sort(self.microtimes[:]))
+            channelwidth = np.unique(differences)[1]
+        self.channelwidth = channelwidth
+        self.tmin = tmin
+        self.tmax = tmax
         self.measured = None
         self.t = None
         self.ignore = False
         self.bg = False
+        self.histogram = None
+        self.binnedtrace = None
+
+    def makehistogram(self):
+        """Put the arrival times into a histogram"""
+
+        self.histogram = Histogram(self)
+
+    def binints(self, binsize):
+        """Bin the absolute times into a trace using binsize"""
+
+        self.binnedtrace = Trace(self, binsize)
 
 
 class Trace:
-    pass
+    """Binned intensity trace
+
+    Parameters
+    ----------
+    particle: Particle
+        The Particle which creates the Trace.
+
+    binsize: float
+        Size of time bin in ms.
+    """
+
+    def __init__(self, particle, binsize):
+
+        self.particle = particle
+        self.binsize = binsize
+        data = self.particle.abstimes[:]
+
+        binsize = binsize * 1000000  # Convert ms to ns
+        endbin = np.int(np.max(data) / binsize)
+
+        binned = np.zeros(endbin)
+        for step in range(endbin):
+            binned[step] = np.size(data[((step + 1) * binsize > data) * (data > step * binsize)])
+
+        # binned *= (1000 / 100)
+        self.intdata = binned
+
+
+class Histogram:
+
+    def __init__(self, particle):
+
+        self.particle = particle
+        tmin = min(self.particle.tmin, self.particle.microtimes[:].min())
+        tmax = max(self.particle.tmax, self.particle.microtimes[:].max())
+        window = tmax - tmin
+        numpoints = int(window // self.particle.channelwidth)
+
+        t = np.linspace(0, window, numpoints)
+        # particle.microtimes -= particle.microtimes.min()
+
+        self.decay, self.t = np.histogram(self.particle.microtimes[:], bins=t)
 
 
 class RasterScan:
@@ -98,9 +134,8 @@ class Spectra:
     pass
 
 
-class Histogram:
-    pass
-
-
 class Levels:
-    pass
+
+    def __init__(self):
+
+        pass  # Change points code called here?
