b32d9d6620bf1b75c810f51ff629b07ec561bef4
Bertus van Heerden <bertus.mooikloof@gmail.com>
2023-02-27 17:18:56 -0800

    Fixed bug where amplitude std was 0. Fixed "value out of bounds" message. Removed some very old code and cleaned up a little bit.

--- src/tcspcfit.py
+++ src/tcspcfit.py
@@ -74,48 +74,6 @@ def makerow(vector):
     return np.reshape(vector, (1, -1))
 
 
-# TODO: simplify this as it is only used by distfluofit()
-def convol(irf, decay):
-    """Performs a convolution of irf with decay.
-
-    Periodicity (period = size(decay)) is assumed.
-
-    Arguments:
-    irf -- row vector or 1D
-    decay -- 1D or array with irf_data in rows
-
-    Output
-    y -- convolution of irf with each row of decay.
-    """
-
-    # Make sure irf (and decay if 1D) are row vectors:
-    irf = makerow(irf)
-    if decay.ndim == 1:
-        irf = makerow(decay)
-
-    # Make irf the same length as decay:
-    mm = np.mean(irf[-11:])
-    decaylength = np.size(decay, 1)
-    irflength = np.size(irf, 1)
-    if decaylength > irflength:
-        irf = np.append(irf, mm * np.ones(decaylength - irflength))
-    else:
-        irf = irf[:, :decaylength]
-
-    # Duplicate rows of irf so dimensions are the same as decay and convolve:
-    y = np.float_(np.real(ifft(np.outer(np.ones(np.size(decay, 0)), fft(irf)) * fft(decay))))
-
-    # t needs to have the same length as irf (irflength) in each case:
-    if irflength <= decaylength:
-        t = np.arange(0, irflength)
-    else:
-        t = np.concatenate((np.arange(0, decaylength), np.arange(0, irflength - decaylength)))
-
-    # Either remove from y or add from start to end so that y has same length as irf:
-    y = y.take(t, 1)
-    return y
-
-
 def colorshift(irf, shift, irflength=None):
     """Shift irf left or right 'periodically'.
 
@@ -142,78 +100,6 @@ def colorshift(irf, shift, irflength=None):
     integer_right_shift = irf[new_index_right]
     irs = (1 - shift + np.floor(shift)) * integer_left_shift + (shift - np.floor(shift)) * integer_right_shift
     return irs
-
-
-# TODO: Add colourshift estimation to this function (important for actually getting correct lifetimes)
-def distfluofit(irf, measured, period, channelwidth, cshift_bounds=[-3, 3], choose=False, ntau=50):
-    """Quickly fit a multiexponential decay to use as 'initial guess'
-
-        The function aims to identify the number of lifetimes in the
-        measured irf_data, as well as the values of the lifetimes. The result
-        can be used as an initial guess for fluofit.
-
-    Arguments:
-    irf -- Instrumental Response Function measured -- Fluorescence decay irf_data
-    period -- Time between laser exciation pulses (in nanoseconds)
-    channelwidth -- Time width of one TCSPC channel (in nanoseconds)
-    tau -- Initial guess times
-    taubounds -- limits for the lifetimes guess times - defaults to 0<tau<100
-                 format: [[tau1_min, tau1_max], [tau2_min, tau2_max], ...]
-    init -- Whether to use a initial guess routine or not
-
-    Output:
-    peak_tau	-- Decay times of the different decay components
-    TODO: Add all other outputs
-    c -- Color Shift (time shift of the IRF w.r.t. the fluorescence curve)
-    offset -- Offset
-    amplitudes -- Amplitudes of the different decay components
-    dc -- Color shift error
-    doffset -- Offset error
-    dtau -- Decay times error
-    irs -- IRF, shifted by the value of the colorshift
-    separated_decays -- Fitted fluorecence component curves
-    t -- time axis
-    chisquared -- chi squared value
-
-    """
-    irf = irf.flatten()
-    irflength = np.size(irf)
-    tp = channelwidth*np.arange(1, period/channelwidth)  # Time index for whole window
-    t = np.arange(irflength)  # Time index for IRF
-    nrange = np.arange(ntau)
-    # Distribution of inverse decay times:
-    tau = (1/channelwidth) / np.exp(nrange / ntau * np.log(period / channelwidth))
-    decays = convol(irf, np.exp(np.outer(tau, -tp)))
-    decays = decays / np.sum(decays)
-    amplitudes, residuals = nnls(decays.T, measured.flatten())
-    tau = 1/tau
-    # print(period)
-    plt.plot(amplitudes)
-    plt.show()
-    peak_amplitudes = amplitudes > 0.1 * np.max(amplitudes)  # Pick out peaks
-    peak_amplitudes = peak_amplitudes.flatten()
-    t = np.arange(1, np.size(peak_amplitudes))
-
-    # t1 are the 'start points' and t2 are the 'end points' of the peaks
-    t1 = t[peak_amplitudes[1:] > peak_amplitudes[:-1]] + 1
-    t2 = t[peak_amplitudes[:-1] > peak_amplitudes[1:]]
-    # Make sure there isn't a peak at the edge:
-    if t1[0] > t2[0]:
-        t2 = np.delete(t2, 0)
-    if t1[-1] > t2[-1]:
-        t1 = np.delete(t1, -1)
-    if np.size(t1) == np.size(t2) + 1:
-        t2 = np.delete(t2, 0)
-    if np.size(t2) == np.size(t1) + 1:
-        t1 = np.delete(t1, -1)
-
-    peak_tau = np.array([])
-    #  Calculate weighted average tau for each peak:
-    for j in range(np.size(t1)):
-        peak_tau = np.append(peak_tau, np.dot(amplitudes[t1[j]-1:t2[j]],
-                                              tau[t1[j]-1:t2[j]]) / np.sum(amplitudes[t1[j]-1:t2[j]]))
-
-    return peak_tau
 
 
 class FluoFit:
@@ -250,8 +136,6 @@ class FluoFit:
         fluorescence decay that is either around 10 times higher than the
         background level or equivalent to 0.1% of the counts in the peak
         channel, whichever is greater.
-    init : bool  # TODO: add this functionality
-        Whether to use an initial guess routine or not.
     ploton : bool
         Whether to automatically plot the irf_data
 
@@ -262,7 +146,6 @@ class FluoFit:
 
         self.numexp = numexp
         self.channelwidth = channelwidth
-        # self.init = init
         self.ploton = ploton
         self.t = t
 
@@ -287,7 +170,6 @@ class FluoFit:
         self.setup_params(amp, shift, tau, fwhm)
         self.settings = Settings()
         self.load_settings()
-
 
         self.bg = None
         self.irfbg = None
@@ -365,9 +247,6 @@ class FluoFit:
         autostart = boundaries[2]
         autoend = boundaries[3]
 
-        if endpoint is None:
-            endpoint = measured.size - 1
-
         startmax = None
         if settings.lt_use_moving_avg:
             measured = moving_avg(vector=measured,
@@ -414,9 +293,9 @@ class FluoFit:
             if settings.lt_use_moving_avg:
                 endpoint += int(np.round(settings.lt_moving_avg_window/2))
         elif endpoint is not None:
-            endpoint = min(endpoint, measured.size)
+            endpoint = min(endpoint, measured.size - 1)
         else:
-            endpoint = measured.size
+            endpoint = measured.size - 1
 
         if settings is not None:
             if channel_width*(endpoint - startpoint) < settings.lt_minimum_decay_window:
@@ -662,9 +541,8 @@ class FluoFit:
         residuals = residuals / np.sqrt(np.abs(convd * self.meas_max))
 
         residualsnotinf = np.abs(residuals) != np.inf
-        residuals = residuals[residualsnotinf]  # For some reason this is the only way i could find that works
+        residuals = residuals[residualsnotinf]  # For some reason this is the only way I could find that works
         chisquared = np.sum((residuals ** 2)) / (np.size(measured) - param_df - 1)
-        # chisquared = chisquared * self.meas_max  # This is necessary because of normalisation
         self.chisq = chisquared
         self.t = self.t[self.startpoint:self.endpoint]
         self.residuals = residuals
@@ -674,11 +552,8 @@ class FluoFit:
         if self.ploton:
             fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
             ax1.set_yscale('log')
-            # ax1.set_ylim([self.measured.min() * 1000, self.measured.max() * 2])
             ax1.plot(self.t, measured.flatten(), color='gray', linewidth=1)
             ax1.plot(self.t, self.convd.flatten(), color='C3', linewidth=1)
-            # ax1.plot(self.irf[self.startpoint:self.endpoint])
-            # ax1.plot(colorshift(self.irf, shift)[self.startpoint:self.endpoint])
             textx = (self.endpoint - self.startpoint) * self.channelwidth * 1.4
             texty = measured.max()
             try:
@@ -686,10 +561,7 @@ class FluoFit:
                 ax1.text(textx, texty / 2, 'Amp = ' + ' '.join('{:#.3g} '.format(F) for F in amp))
             except TypeError:  # only one component
                 ax1.text(textx, texty, 'Tau = {:#.3g}'.format(tau))
-                # ax1.text(textx, texty / 2, 'Amp = {:#.3g}\% '.format(amp))
-                # ax1.text(textx, texty / 2, 'Tau err = %s' % dtau)
             ax2.plot(self.t[residualsnotinf], residuals, '.', markersize=2)
-            # print(residuals.max())
             ax2.text(textx, residuals.max() / 1.1, r'$\chi ^2 = $ {:3.4f}'.format(chisquared))
             ax2.set_xlabel('Time (ns)')
             ax2.set_ylabel('Weighted residual')
@@ -836,7 +708,6 @@ class OneExp(FluoFit):
             else:
                 fwhm = None
 
-            # self.convd = self.fitfunc(self.t[self.startpoint:self.endpoint], tau, amp, shift, fwhm)
             self.convd = self.fitfunc(self.t, tau, amp, shift, fwhm)
             self.results(tau, stds, shift, amp=1, fwhm=fwhm)
 
@@ -872,23 +743,21 @@ class TwoExp(FluoFit):
 
         if addopt is None:
             param, pcov = curve_fit(self.fitfunc, self.t, self.measured, bounds=(paramin, paramax), p0=paraminit)
-            # sigma=np.sqrt(np.abs(self.measured)))
         else:
             param, pcov = curve_fit(self.fitfunc, self.t, self.measured, bounds=(paramin, paramax), p0=paraminit,
                                     **addopt)
 
         tau = param[0:2]
-        # amp = np.append(param[2], param[3])
         amp = np.append(param[2], 1 - param[2])
         shift = param[4]
         stds = np.sqrt(np.diag(pcov))
+        stds[3] = stds[2]  # second amplitude std is same as that of the first
 
         if self.simulate_irf:
             fwhm = param[5]
         else:
             fwhm = None
 
-        # self.convd = self.fitfunc(self.t[self.startpoint:self.endpoint], tau[0], tau[1], amp[0], amp[1], shift, fwhm)
         self.convd = self.fitfunc(self.t, tau[0], tau[1], amp[0], amp[1], shift, fwhm)
         self.results(tau, stds, shift, amp, fwhm)
 
@@ -931,6 +800,7 @@ class ThreeExp(FluoFit):
         amp = np.append(param[3:5], 1 - param[3] - param[4])
         shift = param[6]
         stds = np.sqrt(np.diag(pcov))
+        stds[5] = np.sqrt(stds[3] ** 2 + stds[4] ** 2)  # third amp std is based on first two
 
         if self.simulate_irf:
             fwhm = param[7]
@@ -1227,8 +1097,10 @@ class FittingDialog(QDialog, UI_Fitting_Dialog):
                 bg = FluoFit.estimate_bg(decay, settings=self.settings)
                 start = int(start / channelwidth)
                 end = int(end / channelwidth) if end is not None else None
+                print(end)
                 start, end = FluoFit.calculate_boundaries(decay, [start, end, autostart, autoend],
                                                           bg, self.settings, channelwidth)
+                print(end)
                 if autostart != 'Manual':
                     self.lineStartTime.setText(f'{start * channelwidth:.3g}')
                 if autoend:
